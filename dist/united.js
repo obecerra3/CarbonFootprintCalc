4dd7bf294510252b4131e6fb0a0039cca858a728
tseaver6 <dancerfool@gmail.com>
2019-02-06 12:18:55 -0500

    Working united.js autoscrape

--- dist/united.js
+++ dist/united.js
@@ -2,21 +2,33 @@
 function pull_flight_info() {
 
 	var flight_blocks = $(".flight-block");
+	var flight_dates = $(".flight-summary-date");
+	$('.flight-time-depart').find('span').remove()
+	var flight_departures = $(".flight-time-depart");
+	$('.flight-time-arrive').find('span').remove()
+	var flight_arrivals = $(".flight-time-arrive");
+	var flight_durations = $(".flight-duration");
+	$('.flight-duration').find('span').remove()
+	var flight_origins = $(".flight-station-origin");
+	var flight_destinations = $(".flight-station-destination");
+	$('.segment-flight-number').find('span').remove()
+	var flight_numbers = $(".segment-flight-number");
+	$('.segment-aircraft-type').find('span').remove()
+	var flight_aircrafts = $(".segment-aircraft-type");
 	var flights = [];
-
-	for (int i = 0; i < flight_blocks.length; i++) {
-		let flight_string = "Flight " + i + "\n";
-		flight_string += $(".flight-summary-date") + "\n";
-		flight_string += $(".flight-time-depart") + " " + $(".flight-time-arrive") + "\n";
-		flight_string += "Total Flight Time: " + $(".flight-duration") + "\n";
-		flight_string += $(".flight-station-origin") + " to " + $(".flight-station-destination") + "\n";
-		flight_string += "Flight Number: " + " " + $(".segment-flight-number") + "\n";
-		flight_string += "Flight Aircraft: " + " " + $(".segment-aircraft-type") + "\n";
+	for (var i = 0; i < 2; i++) {
+		let flight_string = "Flight " + (i+1) + "\n";
+		flight_string += $(flight_dates[i]).text() + "\n";
+		flight_string += $(flight_departures[i]).text().trim() + " " + $(flight_arrivals[i]).text().trim() + "\n";
+		flight_string += "Total Flight Time: " + $(flight_durations[i]).text() + "\n";
+		flight_string += $(flight_origins[i]).text() + " to " + $(flight_destinations[i]).text() + "\n";
+		flight_string += "Flight Number: " + " " + $(flight_numbers[i]).text().trim() + "\n";
+		flight_string += "Flight Aircraft: " + " " + $(flight_aircrafts[i]).text().trim() + "\n";
 		flights.push(flight_string);
 	}
-
-
-	alert(flights.join("\n"));
+	for (var j = 0; j < flights.length; j++) {
+		alert(flights[j]);
+	}
 
 }
 
