f63fb534858f4c11b956753411c2a1596191121a
yujitakai <yuji21eagles0912@yahoo.co.jp>
2019-02-16 00:50:25 -0500

    Update american.js

--- american.js
+++ american.js
@@ -1,51 +1,56 @@
-var trips = []
-var flights = []
+/**
+ * classifies the ticket class into the 4 categories described in integers
+ * 0: economy, 1: economy plus, 2: business, 3: first
+ * @param  {string} str the ticket class
+ * @return {number}     the category number of the ticket class
+ */
 function classifyClass(str) {
-    if (str === "Basic Economy" || str === "Economy") {
+    if (str === "Basic Economy" || str === "Main Cabin") {
         return 0;
-    } else if (str === "Premium Economy") {
+    } else if (str === "Premium Economy Flexible") {
         return 1;
     } else if (str === "Business") {
         return 2;
-    } else if (str === "First") {
+    } else if (str === "First Flexible") {
         return 3;
     } else {
         return 0;
     }
 }
+
+/**
+ * scrapes information of the flights from the American Airline's booking
+ * website and generates flightProfile instances used to populate the index.html
+ */
 function scan_flight_data() {
     let trip_summaries = $(".flight-details-summary");
     for (let i = 0; i < trip_summaries.length; i+=1) {
         let trip_summary = $(trip_summaries[i]);
         // Obtaining flight time information
-        // entries in times are in the format: HH:MM AM
         let time_info = $(trip_summary.find("span.flight-time.gamma.aa-strong").first()).text().trim().split('\n');
-        console.log(time_info);
         let hr_min = time_info[0].split(":");
         let am_pm = time_info[1].trim();
         let hr = (am_pm == "PM" && Number(hr_min[0]) !== 12) ? Number(hr_min[0]) + 12 : Number(hr_min[0]);
         let min = Number(hr_min[1]);
         let time = hr.toString() + ":" + min.toString() + ":00";
-        console.log(time)
+
 
         let flight_summary = $(trip_summary.find(".flight-summary"));
         // Obtaining flight date infomation
-        // date is formatted as "Day, Month Date, year"
         let dates = $(flight_summary.find("h3")).text();
         let date_infos = dates.split(", ");
         let date_str = date_infos[1] + ", " + date_infos[2];
-        console.log(date_str + " " + time);
         let date = new Date(date_str + " " + time);
-        console.log(date);
 
         // Obtaining cabin type information
         let ticketClass = classifyClass($(trip_summary.find(".cabin-type")).text().trim());
-        console.log("Class type: " + ticketClass);
+        console.log(ticketClass);
+
+        // Obtaining destination and arrival airports of the overall flight
         let locations = flight_summary.find(".visible-phone");
-        let depart = $(locations[0]).text();
-        // console.log(depart)
-        let arrival = $(locations[1]).text();
-        // console.log(dest);
+        let depart = $(locations[0]).text().trim();
+        let arrival = $(locations[1]).text().trim();
+
 
         let flight_selectors = trip_summary.find(".flight-info");
         if (flight_selectors.length < 1) {
@@ -53,25 +58,24 @@ function scan_flight_data() {
             return;
         } else if (flight_selectors.length == 1) {
             // non stop trip
+            // obtaining flight number
             let flightNo = $(flight_selectors.find(".flight-numbers")).text().trim();
-            console.log(flightNo);
+            // obtaining aircraft model
             let aircraft = $(flight_selectors.find(".wrapText")).text().trim();
-            console.log(aircraft);
             new FlightProfile(flightNo, depart, arrival, date, "American Airlines", aircraft, ticketClass);
 
         } else {
             for (let j = 0; j < flight_selectors.length; j += 1) {
                 let flight = $(flight_selectors[j]);
                 let locs = $(flight.children().first()).text().trim();
-                // console.log(locs);
-                let depart1 = locs.substring(0, 4);
-                // console.log(depart1)
+                // obtaining departure airport IATA code
+                let depart1 = locs.substring(0, 3);
+                // obtaining arrival airport IATA code
                 let arrival1 = locs.substring(6, 9);
-                // console.log(dest1)
+                // obtaining flight number
                 let flightNo = $(flight.find(".flight-numbers")).text().trim();
-                // console.log(flightNo);
+                // obtaining aircraft model
                 let aircraft = $(flight.find(".wrapText")).text().trim();
-                // console.log(aircraft);
                 new FlightProfile(flightNo, depart1, arrival1, date, "American Airlines", aircraft, ticketClass);
 
             }
