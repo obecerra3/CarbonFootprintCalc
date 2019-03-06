b5daa83dfdf5e7d55fca9a1e4ee64235defab536
CameronCacic <ccacic0@gmail.com>
2019-03-05 23:39:19 -0500

    revamped to be more efficient, have cleaner code, and handle multipart layover flights

--- delta.js
+++ delta.js
@@ -29,15 +29,22 @@ function parseMonth(monthStr) {
 	return -1;
 }
 
-function parseDate(dateStr, timeStr) {
+
+function parseDetailString(detailStr) {
+	// removes all spaces and splits around continuous non-space strings
+	return detailStr.match(/[^ ]+/g);
+}
+
+function parseDate(detailStr) {
 		
-	if (!dateStr || dateStr === "" || !timeStr || timeStr === "") {
+	if (!detailStr || detailStr === "") {
 		return null;
 	}
 	
-	let splitIndex = dateStr.search("[0-9]");
-	let dayStr = dateStr.substring(splitIndex, splitIndex + 2);
-	let monthStr = dateStr.substring(splitIndex + 2);
+	let splitStr = parseDetailString(detailStr);
+	
+	let dayStr = splitStr[2];
+	let monthStr = splitStr[3].toUpperCase();
 	
 	let day = parseInt(dayStr);
 	let month = parseMonth(monthStr);
@@ -45,10 +52,10 @@ function parseDate(dateStr, timeStr) {
 	let currDate = new Date();
 	let year = currDate.getFullYear();
 	
-	let splitTime = timeStr.split(":")
+	let splitTime = splitStr[4].split(":");
 	let hour = parseInt(splitTime[0]);
 	let minute = parseInt(splitTime[1]);
-	if (splitTime[1].search("PM") != -1) {
+	if (splitStr[5].search("PM") != -1) {
 		if (hour != 12) {
 			hour += 12;
 		}
@@ -65,76 +72,91 @@ function parseDate(dateStr, timeStr) {
 	
 };
 
+function parseAirport(detailStr) {
+	if (!detailStr || detailStr === "") {
+		return null;
+	}
+	let splitStr = parseDetailString(detailStr);
+	let airport = splitStr[splitStr.length - 1];
+	return airport.substring(1, airport.length - 1);
+}
+
+function parseFlightNo(detailStr) {
+	if (!detailStr || detailStr === "") {
+		return null;
+	}
+	let splitStr = parseDetailString(detailStr);
+	return splitStr[1] + " " + splitStr[2];
+}
+
+function parseAircraft(detailStr) {
+	
+	if (!detailStr || detailStr === "") {
+		return null;
+	}
+	
+	let splitStr = parseDetailString(detailStr);
+	let aircraft = splitStr[1];
+	let i = 2;
+	while (i < splitStr.length && splitStr[i] != "View") {
+		aircraft += " " + splitStr[i];
+		i += 1;
+	}
+	
+	return aircraft;
+}
+
 function pullFlightInfo() {
-	let flightNoElems = $(".fltNumber");
-	let flightNos = [];
-
-	flightNoElems.each(function(index) {
-		let flightNo = $(this).text();
-		flightNos.push(flightNo);
-	});
-
-	let tripTimeElems = $(".tripTime");
-	let flightDateElems = $(".tripRowDate");
-	let flightDates = [];
-	flightDateElems.each(function(index) {
-		let flightDate = $(this).text();
-		var flightTime = null;
-		tripTimeElems.each(function(index2) {
-			if (index2 === 0) {
-				flightTime = $(this).html();
-			}
-		});
-		flightDate = parseDate(flightDate, flightTime);
-		flightDates.push(flightDate);
-	});
 
 	let detailElems = $(".detailsRowItem");
 	let flightAircrafts = [];
-	detailElems.each(function(index) {
-		
-		if ((index + 1) % 4 === 0) {
-			let spans = $(this).find('span');
-			spans.each(function(index2) {
-				
-				if (index2 === 1) {
-					let aircraft = $(this).text();
-					flightAircrafts.push(aircraft);
-				}
-				
-			});
-		}
-	});
-
-	let tripSummaryFlyElems = $(".tripSummeryFly");
 	let flightDepart = [];
 	let flightArrive = [];
-	var first = true;
-	tripSummaryFlyElems.each(function(index) {
-		let value = $(this).html();
-		if (value.length === 3) {
-			if (first) {
-				flightDepart.push(value);
-				first = false;
-			} else {
-				flightArrive.push(value);
-				first = true;
-			}
+	let flightNos = [];
+	let flightDates = [];
+	detailElems.each(function(index) {
+		let modIndex = index % 4;
+		
+		detailStr = $(this).text();
+		
+		if (modIndex === 0) {
+			flightDates.push(parseDate(detailStr));
+			flightDepart.push(parseAirport(detailStr));
 		}
+		
+		if (modIndex === 1) {
+			flightArrive.push(parseAirport(detailStr));
+		}
+		
+		if (modIndex === 2) {
+			flightNos.push(parseFlightNo(detailStr));
+		}
+		
+		if (modIndex === 3) {
+			flightAircrafts.push(parseAircraft(detailStr));
+		}
+		
 	});
 
 	let secLineElems = $(".secLine");
 	let flightClasses = [];
 	secLineElems.each(function(index) {
-		let flightClass = $(this).text();
-		if (flightClass.search("Main Cabin") != -1) {
-			flightClasses.push(1);
-		} else if (flightClass.search("Delta Comfort") != -1) {
-			flightClasses.push(2);
-		} else if (flightClass.search("First Class") != -1) {
-			flightClasses.push(3);
-		} else {
-			flightClasses.push(0);
+		
+		let classes = $(this).text().split("|");
+		for (var i = 0; i < classes.length; i += 1) {
+			
+			let flightClass = classes[i];
+			
+			if (flightClass.search("Main Cabin") != -1) {
+				flightClasses.push(1);
+			} else if (flightClass.search("Delta Comfort") != -1) {
+				flightClasses.push(2);
+			} else if (flightClass.search("First Class") != -1) {
+				flightClasses.push(3);
+			} else {
+				flightClasses.push(0);
+			}
+			
 		}
 	});
 	
@@ -144,4 +166,4 @@ function pullFlightInfo() {
 	}
 }
 
-$(document).ready(pullFlightInfo);
+$(window).bind("load", pullFlightInfo);
